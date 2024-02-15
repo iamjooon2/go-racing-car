e31f6c8787f44071a4bf004454a6d533dcc3d3a8
iamjooon2 <iamjooon2@gmail.com>
2024-02-15 23:00:58 +0900

    test: 예시 코드 bdd 스타일 추석 추가

--- test/application_test.go
+++ test/application_test.go
@@ -9,8 +9,13 @@ import (
 
 // 예시 테스트 함수, 구현 시 제거
 func TestHello(t *testing.T) {
+	// given
 	expected := "Hello, Racing Car!"
+
+	// when
 	actual := racingcar.Hello()
+
+	// then
 	assert.Equal(
 		t,
 		expected,
